37d718a45393f287d47dbc0ec727d8256297c82b
MarkoKasalica <97064713+MarkoKasalica@users.noreply.github.com>
2022-01-19 19:28:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('button');
+let div = document.querySelector('div');
+
+div.style.display = 'none';
+
+button.onclick = () => {
+	div.style.display = 'block';
+};
